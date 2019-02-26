1a9e8f1f4c09d57c4b9c80a6d2f8b6d9c31c1ad6
David Vaca <david1986@gmail.com>
2019-02-25 20:37:01 -0500

    Version inicial talleres en la nube

---
+++ database/database.js
@@ -0,0 +1,23 @@
+var pg = require("pg");
+
+const { Pool } = require('pg')
+
+var config = {
+  //user: 'postgres',
+  //database: 'TalleresBD', 
+  //password: 'postgres123', 
+  user: 'yblnujhratywfx',
+  database: 'd7u53hecedfq97', 
+  password: '5a8bc60b8d58c11e2af2658e956f40f2bd2f934801d7e217f2521784bec26755', 
+  port: 5432, 
+  max: 10, // max number of connection can be open to database
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+const pool = new Pool(config);
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  }
+}
